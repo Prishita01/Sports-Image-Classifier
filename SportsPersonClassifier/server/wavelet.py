1917dcaf8f960361ab6829705f3169659724ce1b
Prishita G K <prishpradh01@gmail.com>
2023-07-23 16:19:37 +0530

    adding .ipynb file

---
+++ SportsPersonClassifier/server/wavelet.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pywt
+import cv2
+
+
+def w2d(img, mode='haar', level=1):
+    imArray = img
+
+    # Datatype conersions, Convert to grayscale
+    imArray = cv2.cvtColor(imArray, cv2.COLOR_RGB2GRAY)
+
+    # Convert to float
+    imArray = np.float32(imArray)
+    imArray /= 255;
+
+    # Compute coefficients
+    coeffs = pywt.wavedec2(imArray, mode, level=level)
+
+    # Process coeffients
+    coeffs_H = list(coeffs)
+    coeffs_H[0] *= 0;
+
+    # Reconstruction
+    imArray_H = pywt.waverec2(coeffs_H, mode);
+    imArray_H *= 255;
+    imArray_H = np.uint8(imArray_H)
+
+    return imArray_H
